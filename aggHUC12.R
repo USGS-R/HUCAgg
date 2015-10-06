1c699254b251a21cbd2d9c1fded06a62a3adc9f3
David Blodgett <dblodgett@usgs.gov>
2015-10-06 14:58:04 -0500

    a bit of cleanup

--- aggHUC12.R
+++ aggHUC12.R
@@ -1,52 +1,60 @@
 library(maptools)
 library(snow)
 
-workingPath<-'~/Desktop/'
+# Set this to where the files are.
+workingPath<-'~/temp'
 
 setwd(workingPath)
 
-# at<-read.csv('huc12_atts.csv', colClasses='character')
-# hucPoly<-readShapePoly("WBDHU12.shp",proj4string= CRS('+init=epsg:4269'))
+# Load directly from shapefile
+hucPoly<-readShapePoly("WBDHU12.shp",proj4string= CRS('+init=epsg:4269'))
 
-load('hucPoly.rda')
+# Or load from an old data export. (faster)
+# load('hucPoly.rda')
 
-# fromHUC<-list()
+# Build the fromHUC list
 # fromHUC<-sapply(hucPoly@data$HUC,fromHUC_finder,hucs=hucPoly@data$HUC,tohucs=hucPoly@data$TOHUC)
 
+# Or load it from a previous run
 load('fromHUC.rda')
 
+# Generate the aggregate HUC list for everything.
 # hucList<-hucPoly@data$HUC
 # aggrHUCs<-sapply(as.character(unlist(hucList)), HUC_aggregator, fromHUC=fromHUC)
 
+# Or load it from a previous run.
 load('aggrHUCs.rda')
 
 ## TESTING One Watershed ##
-# huc<-"070700051802" #Wisconsin River
-# # huc<-"020402040000" #Delaware River
-# # huc<-"150301070105" #Colorado River
-# 
-# outHuc<-unionHUC(huc,aggrHUCs,hucPoly)
-# outShp<-subset(hucPoly,hucPoly@data$HUC %in% huc)
-# outShp@polygons[which(outShp@data$HUC %in% huc)][[1]]<-outHuc@polygons[[1]]
-# writePolyShape(outShp,file.path(huc))
+
+huc<-"070700051802" #Wisconsin River
+# huc<-"020402040000" #Delaware River
+# huc<-"150301070105" #Colorado River
+
+outHuc<-unionHUC(huc,aggrHUCs,hucPoly)
+outShp<-subset(hucPoly,hucPoly@data$HUC %in% huc)
+outShp@polygons[which(outShp@data$HUC %in% huc)][[1]]<-outHuc@polygons[[1]]
+writePolyShape(outShp,file.path(huc))
+
 ## TESTING One Watershed ##
 
 ## TESTING HUC04 ##
-# hucList<-c()
-# for(huc in hucPoly@data$HUC) {
-#   if(grepl('^0707.*',huc)) {
-#     hucList<-c(hucList,huc)
-#   }
-# }
-# outHucs<-sapply(unlist(hucList), unionHUC, upstreamHUCs=aggrHUCs, hucPoly=hucPoly)
-# outShp<-subset(hucPoly,hucPoly@data$HUC %in% hucList)
-# for(huc in outShp@data$HUC){
-#   if(!is.null(outHucs[[huc]])){
-#     print(huc)
-#     outShp@polygons[which(outShp@data$HUC %in% huc)][[1]]<-outHucs[[huc]]@polygons[[1]]
-#   }
-# }
-# writePolyShape(outShp,file.path('0707'))
+
+hucList<-c()
+for(huc in hucPoly@data$HUC) {
+  if(grepl('^0707.*',huc)) {
+    hucList<-c(hucList,huc)
+  }
+}
+outHucs<-sapply(unlist(hucList), unionHUC, upstreamHUCs=aggrHUCs, hucPoly=hucPoly)
+outShp<-subset(hucPoly,hucPoly@data$HUC %in% hucList)
+for(huc in outShp@data$HUC){
+  if(!is.null(outHucs[[huc]])){
+    print(huc)
+    outShp@polygons[which(outShp@data$HUC %in% huc)][[1]]<-outHucs[[huc]]@polygons[[1]]
+  }
+}
+writePolyShape(outShp,file.path('0707'))
 
 ## TESTING HUC08 ##
 
